cf1936bafe3ee1efa879b506e20699ebd3b1318c
Diego José da Silva <diegojsilvabr@gmail.com>
2020-05-01 23:18:46 -0300

    add test for make_average

---
+++ test/context.py
@@ -0,0 +1,8 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
+# import models
+from algorithms import plsr
+from utils import average
