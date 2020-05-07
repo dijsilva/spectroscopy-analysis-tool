80510de435828822c5a2c6c622589175fdcc4d21
Diego José da Silva <diegojsilvabr@gmail.com>
2020-05-07 19:40:54 -0300

    add random forest

--- teste.py
+++ teste.py
@@ -1,13 +1,21 @@
-from algorithms.regression import PCR
+from algorithms.regression import PCR, RandomForest, PLSR
 from algorithms.classification import PCA_LDA
 from transformations import snv, sg, msc
 import pandas as pd
 from utils import make_average
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 # regression
-#df = pd.read_csv('/home/dsilva/teste_pcr/foss_para_arff_calibracao.csv', sep=';', decimal=',')
-#df_val = pd.read_csv('/home/dsilva/teste_pcr/foss_para_arff_validacao.csv', sep=';', decimal=',')
+df = pd.read_csv('/home/dsilva/teste_pcr/foss_para_arff_calibracao.csv', sep=';', decimal=',')
+df_val = pd.read_csv('/home/dsilva/teste_pcr/foss_para_arff_validacao.csv', sep=';', decimal=',')
+
+#df_average = make_average(df, 2, 2)
+#df_val_average = make_average(df_val, 2, 2)
+
+
+rf = RandomForest(df, 100, 'loo', dataset_validation=df_val)
+rf.create_model()
+
 
 #r2 = []
 #rmse = []
@@ -21,11 +29,11 @@ from utils import make_average
 
 
 #classification
-df = pd.read_csv('/home/dsilva/teste_pcr/espectros_frutos_polpa.csv', sep=';', decimal=',')
-df_test = pd.read_csv('/home/dsilva/teste_pcr/espectros_frutos_polpa1.csv', sep=';', decimal=',')
+# df = pd.read_csv('/home/dsilva/teste_pcr/espectros_frutos_polpa.csv', sep=';', decimal=',')
+# df_test = pd.read_csv('/home/dsilva/teste_pcr/espectros_frutos_polpa1.csv', sep=';', decimal=',')
 
-df_average = make_average(df, 3, 2)
-df_test_average = make_average(df_test, 3, 2)
+# df_average = make_average(df, 3, 2)
+# df_test_average = make_average(df_test, 3, 2)
 
-lda = PCA_LDA(df_average, 8, 'loo', data_validation=df_test_average)
-lda.create_model()
+# lda = PCA_LDA(df_average, 8, 'loo', data_validation=df_test_average)
+# lda.create_model()
